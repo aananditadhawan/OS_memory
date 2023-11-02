ddcd1bf9c809f5fa3b68d26669772dc2f6e685e6
Aanandita Dhawan <dhawan6@wisc.edu>
2023-11-01 20:58:37 -0500

    Working mmap - TC 1

---
+++ xv6-public/mmap.h
@@ -0,0 +1,20 @@
+/* Define mmap flags */
+#define MAP_PRIVATE 0x0001
+#define MAP_SHARED 0x0002
+#define MAP_ANONYMOUS 0x0004
+#define MAP_ANON MAP_ANONYMOUS
+#define MAP_FIXED 0x0008
+#define MAP_GROWSUP 0x0010
+
+/* Protections on memory mapping */
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+
+struct map {
+    void *addr;
+    int length; 
+    int prot;
+    int flags; 
+    int fd;
+    int offset;
+};
---
+++ xv6-public/sysproc.c
@@ -0,0 +1,199 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+//#include "mmap.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+static pte_t *
+walkpgdir(pde_t *pgdir, const void *va, int alloc)
+{
+  pde_t *pde;
+  pte_t *pgtab;
+
+  pde = &pgdir[PDX(va)];
+  if(*pde & PTE_P){
+    pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+  } else {
+    if(!alloc || (pgtab = (pte_t*)kalloc()) == 0)
+      return 0;
+    // Make sure all those PTE_P bits are zero.
+    memset(pgtab, 0, PGSIZE);
+    // The permissions here are overly generous, but they can
+    // be further restricted by the permissions in the page table
+    // entries, if necessary.
+    *pde = V2P(pgtab) | PTE_P | PTE_W | PTE_U;
+  }
+  return &pgtab[PTX(va)];
+}
+
+static int
+mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm)
+{
+  char *a, *last;
+  pte_t *pte;
+
+  a = (char*)PGROUNDDOWN((uint)va);
+  last = (char*)PGROUNDDOWN(((uint)va) + size - 1);
+  for(;;){
+    if((pte = walkpgdir(pgdir, a, 1)) == 0)
+      return -1;
+    if(*pte & PTE_P)
+      panic("remap");
+    *pte = pa | perm | PTE_P;
+    if(a == last)
+      break;
+    a += PGSIZE;
+    pa += PGSIZE;
+  }
+  return 0;
+}
+
+int sys_mmap(void) {
+  //struct map *mm;
+
+  int length, prot, flags, fd, offset, addr;
+
+  // if(argptr(0, (void*)&addr, sizeof(*addr))
+  if(argint(0, &addr) < 0 || argint(1, &length) < 0 || argint(2, &prot) < 0 || 
+  argint(3, &flags) < 0 || argint(4, &fd) < 0 || argint(5, &offset) < 
+  0)
+    return -1;
+    //return (void *)-1;
+
+  // if(flags != MAP_ANON) {
+  //   // Do file based memory mapping
+  // }
+
+  // if((flags & (MAP_ANONYMOUS | MAP_FIXED | MAP_SHARED)) != (MAP_ANONYMOUS | MAP_FIXED | MAP_SHARED))
+  //   //return (void*)-1;
+  //   return -1;
+
+  if(addr < 0x60000000 || addr > 0x80000000)
+    //return (void*)-1;
+    return -1;
+
+  //return 0;
+
+
+  //int pages = PGROUNDUP(length);
+
+  char *mem = kalloc();
+
+  struct proc *curproc = myproc();
+
+  if(mappages(curproc->pgdir, (void*)addr, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+    cprintf("allocuvm out of memory (2)\n");
+    kfree(mem);
+    return 0;
+  }
+  
+  // for(int i=0; i<pages; i++) {
+  //   mem = kalloc();
+  //   if(mem == 0){
+  //     cprintf("allocuvm out of memory\n");
+  //     //deallocuvm(pgdir, newsz, oldsz);
+  //     return 0;
+  //   }
+  //   memset(mem, 0, PGSIZE);
+  //   if(mappages(pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+  //     cprintf("allocuvm out of memory (2)\n");
+  //     deallocuvm(pgdir, newsz, oldsz);
+  //     kfree(mem);
+  //     return 0;
+  //   }
+  // }
+
+  return addr;
+}
+
+int sys_munmap(void) {
+
+  
+}
